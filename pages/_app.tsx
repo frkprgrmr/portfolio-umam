aad0f0d82b6646137e023139a95aff36c14dec7d
khoerul <khoerul@zero-one-group.com>
2022-11-28 11:06:38 +0700

    fix layouting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,16 @@
-import '@fontsource/poppins/400.css';
-import "@fontsource/fira-code/400.css"
+import '@fontsource/poppins/400.css'
+import '@fontsource/fira-code/400.css'
 
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from "@chakra-ui/react";  
-import Layout from '../components/layouts/layout';
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layouts/layout'
 import theme from '../styles/theme'
-import Main from './main';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Layout>
-        <Main/>
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
